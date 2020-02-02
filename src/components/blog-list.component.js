a1529dbab9b7db6b8a78fbe459e057048fbe916a
sharlyfe <behnamsharlyfe@gmail.com>
2020-02-02 18:06:06 -0500

    finished the blog stuff and all of them are working and connected to the mongodb database

--- src/components/blog-list.component.js
+++ src/components/blog-list.component.js
@@ -1,10 +1,74 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+const Blog = props => (
+  <tr>
+    <td>{props.blog.username}</td>
+    <td>{props.blog.description}</td>
+    <td>{props.blog.author}</td>
+    <td>{props.blog.title}</td>
+    <td>{props.blog.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.blog._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteBlog(props.blog._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 export default class BlogList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteBlog = this.deleteBlog.bind(this);
+    this.state = { blogs: [] };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/blogs/")
+      .then(Response => {
+        this.setState({ blogs: Response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  deleteBlog(id) {
+    axios
+      .delete("http://localhost:5000/blogs/" + id)
+      .then(res => console.log(res.data));
+    this.setState({
+      blogs: this.state.blogs.filter(el => el._id !== id)
+    });
+  }
+  blogList() {
+    return this.state.blogs.map(blogs => {
+      return <Blog blog={blogs} deleteBlog={this.deleteBlog} key={blogs._id} />;
+    });
+  }
   render() {
     return (
       <div>
-        <p>you are in blofg list component</p>
+        <h3>Logged Blogs</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Author</th>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.blogList()}</tbody>
+        </table>
       </div>
     );
   }
--- src/components/edit-blog.component.js
+++ src/components/edit-blog.component.js
@@ -1,9 +1,182 @@
 import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 export default class EditBlog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onChangeAuthor = this.onChangeAuthor.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      date: new Date(),
+      title: "",
+      author: "",
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/blogs/" + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          title: response.data.title,
+          author: response.data.author,
+          date: new Date(response.data.date)
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    axios
+      .get("http://localhost:5000/users/")
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username)
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+  onChangeAuthor(e) {
+    this.setState({
+      author: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const blog = {
+      username: this.state.username,
+      description: this.state.description,
+      title: this.state.title,
+      date: this.state.date,
+      author: this.state.author
+    };
+
+    console.log(blog);
+
+    axios
+      .post(
+        "http://localhost:5000/blogs/update/" + this.props.match.params.id,
+        blog
+      )
+      .then(res => console.log(res.data));
+
+    // window.location = "/";
+  }
+
   render() {
     return (
       <div>
-        <p>you are in edit blog component</p>
+        <h3>Edit BLog</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Author: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.author}
+              onChange={this.onChangeAuthor}
+            />
+          </div>
+          <div className="form-group">
+            <label>Title: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.onChangeTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Blog"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
